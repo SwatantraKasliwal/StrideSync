1b5d0f2e5feddae0c262bbe8002df66db1a61e64
Swatantra Kasliwal <swatantrakasliwal25@gmail.com>
2025-09-16 15:09:02 +0000

    To implement this change, you need to modify your app's code and Firebas

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
-import AuthProvider from '@/components/auth-provider';
+import Dashboard from '@/components/dashboard';
 
 export default function Home() {
   return (
-    <AuthProvider />
+    <Dashboard />
   );
 }
--- src/app/signup/page.tsx
+++
@@ -1,5 +0,0 @@
-import AuthForm from '../login/auth-form';
-
-export default function SignupPage() {
-  return <AuthForm mode="signup" />;
-}
